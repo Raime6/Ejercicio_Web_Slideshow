0768b7f940283948bc95c4a2d23e03bc8f25c2c9
DESKTOP-65SAUST\Alumno <informatica@esne.es>
2024-11-20 15:44:15 +0100

    Auto slideShow
    As a function in JS

--- slideshow_INCOMPLETO/js/slideshow.js
+++ slideshow_INCOMPLETO/js/slideshow.js
@@ -14,7 +14,27 @@
 
 // Initialize a slideIndex container
 let slideIndex = 1;
+let autoSlide;
+let dots       = document.getElementsByClassName("dot");
+
+// Adds to every dot a click event
+for (let i = 0; i < dots.length; i++)
+{
+    const dot = dots[i];
+    dot.addEventListener("click", function()
+    {
+        
+        stopAutoSlide();
+        currentSlide(i + 1);
+        startAutoSlide();
+    })
+}
+
+// Shows first slide when charging web
 showSlide(slideIndex);
+
+// Initites auto performance of slideshow
+startAutoSlide();
 
 // We select ther arrows for next and prev slide
 let prevSlideArrow = document.querySelector(".prev");
@@ -22,12 +42,16 @@ let nextSlideArrow = document.querySelector(".next");
 
 prevSlideArrow.addEventListener("click", function()
 {
+    stopAutoSlide();
     nextPrevSlide(-1);
+    startAutoSlide();
 });
 
 nextSlideArrow.addEventListener("click", function()
 {
+    stopAutoSlide();
     nextPrevSlide(1);
+    startAutoSlide();
 });
 
 /**
@@ -43,15 +67,27 @@ function nextPrevSlide(n)
 }
 
 /**
+ * Click on dots updates the slideIndex
  * 
- * @param {"type: slideNumber"} slideNum
+ * @param {number} n
+ */
+function currentSlide(n)
+{
+    console.log(n);
+    
+    slideIndex = n;
+    showSlide(slideIndex);
+}
+
+/**
+ * 
+ * @param {number} slideNum
  * 
  * This functions shows a specific slide passed as an argument 
  */
 function showSlide(slideNum)
 {
     let slides = document.querySelectorAll(".mySlides");
-    let dots   = document.querySelectorAll(".dot");
 
     if (slideIndex > slides.length)
         slideIndex = 1;
@@ -59,12 +95,32 @@ function showSlide(slideNum)
     if (slideNum < 1)
         slideIndex = slides.length;
 
-    for (i = 0; i < slides.length; ++i)
+    for (j = 0; j < slides.length; ++j)
     {
-        slides[i].style.display = "none";
-        dots[i].className = dots[i].className.replace("active", "");
+        slides[j].style.display = "none";
+        dots[j].className = dots[j].className.replace("active", "");
     }
 
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
+}
+
+
+/**
+ * Automates nextPrevSlide() function call using the built-in setInterval() function
+ * 
+ * Value 1 as enter paramenter to pass to the next slide
+ */
+function startAutoSlide()
+{
+    autoSlide = setInterval(function()
+    {
+        nextPrevSlide(1);
+    }, 3000); // Sets time between calls in miliseconds
+}
+
+// Stops auto performance of slideShow
+function stopAutoSlide()
+{
+    clearInterval(autoSlide);
 }
